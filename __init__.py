cb7ade167ef147568ab09f51213ca491695b0dda
maximosanchezpccomp <maximo.sanchez@pccomponentes.com>
2025-09-01 13:07:19 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,24 @@
+"""Semantic SEO Analyzer
+
+Una aplicación avanzada para analizar el contenido semántico de las páginas web 
+y optimizarlas para los algoritmos modernos de motores de búsqueda, con énfasis 
+en la comprensión semántica y el E-E-A-T.
+
+Este paquete proporciona herramientas para:
+1. Análisis semántico de URL
+2. Puntuación de relevancia semántica de palabras clave
+3. Recomendaciones para mejora de contenido basadas en semántica
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Semantic SEO Analyzer Team'
+
+# Import the main packages
+import modules
+import utils
+import config
+
+# Define application version and metadata
+APP_NAME = 'Semantic SEO Analyzer'
+APP_VERSION = __version__
+APP_DESCRIPTION = 'Herramienta de análisis semántico para SEO avanzado'
